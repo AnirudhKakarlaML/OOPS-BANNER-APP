dcef1b1249cccdd5567bc05bfe75421f15b05e1e
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2026-03-02 13:47:11 +0530

    Add files via upload

---
+++ week12.java
@@ -0,0 +1,27 @@
+// Creating Class with name DisplayResult indicating the purpose
+// is to display the student's result. Class name is a Noun.
+
+class week12 {
+
+    public static void main(String[] args) {
+
+        // Create a string variable name and assign value Sam
+        String name = "Sam";
+
+        // Create an int variable rollNumber and assign value 1
+        int rollNumber = 1;
+
+        // Create a double variable percentMarks and assign value 99.99
+        double percentMarks = 99.99;
+
+        // Create a char variable result and assign value 'P'
+        // 'P' indicates Pass and 'F' indicates Fail
+        char result = 'P';
+
+        // Display the result
+        System.out.println("Displaying Result:");
+        System.out.println(name + " with Roll Number " + rollNumber +
+                " has scored " + percentMarks +
+                "% Marks and Result is " + result);
+    }
+}
